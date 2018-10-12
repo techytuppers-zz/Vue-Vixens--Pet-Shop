5c29f9c0537a2da0d9a610ca035478801fc6b1f8
Sarah Tupman <sarahtupman@gmail.com>
2018-10-12 23:40:57 +0100

    Chapter 2
    https://vuevixens.github.io/docs/workshop/ch2.html#instructions

---
+++ src/components/Dog.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card color="grey lighten-2">
+    <v-img :src="dog.img" height="170px">
+    </v-img>
+    <v-card-title>
+      <div>
+        <h3>{{dog.name}}</h3>
+        <p>{{dog.breed}}</p>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    dog: {
+      type: Object
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,26 @@ import Vue from "vue";
 import App from "./App";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import VueRouter from "vue-router";
+
+import Home from "./views/Home";
+import Pets from "./views/Pets";
+
+// Routing
+const routes = [
+  { path: "/", component: Home },
+  { path: "/pets", component: Pets }
+];
+const router = new VueRouter({ routes });
 
 Vue.use(Vuetify);
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
   components: { App },
-  template: "<App/>"
+  template: "<App/>",
+  router
 });

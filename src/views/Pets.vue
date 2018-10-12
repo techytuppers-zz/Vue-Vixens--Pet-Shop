17a5faf2a0f378488202b72691cc4c8ea5ae6088
Sarah Tupman <sarahtupman@gmail.com>
2018-10-13 00:19:52 +0100

    Chapter 3
    https://vuevixens.github.io/docs/workshop/ch3.html

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -9,8 +9,10 @@
 </template>
 
 <script>
+  import axios from "axios";
   import {Dogs} from "../data/dogs";
   import Dog from '../components/Dog.vue';
+  axios.defaults.baseURL = "https://dog.ceo/api";
   export default {
     components: {
       appDog: Dog,
@@ -19,6 +21,33 @@
       return {
         dogs: Dogs
       };
+    },
+    created() {
+      axios
+      .get("/breed/husky/images/random")
+      .then(response => {
+        // console.log(response);
+        // const husky = this.dogs.find(dog => dog.breed === 'husky');
+        // console.log(husky);
+        // husky.img = response.data.message;
+        this.dogs.forEach(dog => {
+          dog.img = "";
+        });
+        const linksArray = this.dogs.map(
+          dog => "/breed/" + dog.breed + "/images/random"
+        );
+        axios.all(linksArray.map(link => axios.get(link)))
+        .then(
+          axios.spread((...res) => {
+            this.dogs.forEach((dog, index) => {
+              dog.img = res[index].data.message;
+            });
+          })
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
    };
 </script>
